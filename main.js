c3aec973a174ede45928d12a240e73c3ee0b834a
joebit213 <joseph@evatic.org>
2018-09-06 01:24:53 -0500

    version 2.1

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
-//canvas config
+
+
 var canvas = document.getElementsByTagName('canvas')[0];
 var ctx = canvas.getContext('2d')
 
-//testing
-//ctx.fillRect(0,0,50,50)
 
-
-//Variables globales
-var zombiess = []
+//variables globales
+var zombies = []
 var cuervos = []
+var bombas = []
+var score1 = 0;
+var score2 = 0;
 var interval;
 var frames = 0;
-var disparosP = []
 var images = {
   fondo:'https://art.pixilart.com/b1cba2e0287f955.png',
   comandante :'https://art.pixilart.com/f918c10aafbd1c5.png',
   piso: 'https://art.pixilart.com/b443ba4e271a644.png',
   pajaro: 'https://art.pixilart.com/f8ad179027578d7.png',
   zombie: 'https://art.pixilart.com/22d203f96b0d188.png',
-  balas: 'https://art.pixilart.com/d4308951377cb66.png'
+  balas: 'https://art.pixilart.com/d4308951377cb66.png',
+  bomba: 'https://vignette.wikia.nocookie.net/growtopia/images/3/36/Big_bomb.png/revision/latest?cb=20131231104254',
+  boss: 'https://steamusercontent-a.akamaihd.net/ugc/858353348243107330/26976D5F26C812DFCE4E2E84C3250E86E2F8CA3B/'
 }
 
-//clases
 class Piso{
   constructor(){
     this.x = 0
@@ -33,17 +34,12 @@ class Piso{
     this.image.onload = () =>{ 
       this.draw()
     }
-    this.music = new Audio()
-    this.music.src = "http://66.90.93.122/ost/legend-of-zelda-the-majora-s-mask-3d-original-sound-track/dulyjzpw/216%20Ghost%20Attack.mp3"
-  }
+    }
   draw(){
     this.x--
     if(this.x < -canvas.width) this.x = 0;
     ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
     ctx.drawImage(this.image,this.x + this.width,this.y,this.width,this.height)
-
-    //ctx.font = '50px Avenir'
-    //ctx.fillText(Math.floor(frames / 60),100,100)
   }
 }//termina piso
 
@@ -58,102 +54,90 @@ class Fondo{
     this.image.onload = () =>{ 
       this.draw()
     }
+    this.music = new Audio()
+    this.music.src = "http://66.90.93.122/ost/super-smash-bros.-for-nintendo-3ds-and-wii-u-vol-26.-street-fighter/movwjvzt/05.%20Ryu%20Stage%20Type%20B.mp3"
   }
   draw(){
     ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
   }
 }//termina fondo
 
-class Comandante{
-  constructor(){
-    this.x = 20
-    this.y = 250
-    this.width = 80
-    this.height = 120
-    this.image = new Image ()
-    this.image.src = images.comandante
-    this.image.onload = () =>{
-      this.draw()
-    }
-    this.gravity = 4
-    this.crash = new Audio()
-    this.crash.src = "http://66.90.93.122/ost/the-legend-of-zelda-nes/ssgnhwrp/10%20Ending.mp3"
-  }
-  draw(){
-  if(this.y < canvas.height - 260) this.y += this.gravity
-    ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
-  }
-  goRight(){
-  this.x+=15   
-  }
-  goLeft(){
-  this.x-=15
-  }
-  crashWidth(item){
-    var crash = (this.x < item.x + item.width) &&
-           (this.x + this.width > item.x) &&
-           (this.y < item.y + item.height) &&
-           (this.y + this.height > item.y);
-           //if (crash) this.crash.play()
-           return crash;
-  }
-}//termina comandante
-
 class Balas{
-  constructor(){
-    this.x = comand.x+comand.width-10
-    this.y = comand.y+(comand.height/2)-21
-    this.width = 20
-    this.height = 20
-    this.image = new Image ()
+  constructor(x=0,y=0){
+    this.x = x
+    this.y = y
+    this.width=50
+    this.height=50
+    this.image = new Image()
     this.image.src = images.balas
-    this.image.onload = () =>{
+        }
+        draw(){
+            this.x+=10
+            ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
+        }
+    }
+
+class Character{
+        constructor(){
+            this.balas = []
+            this.x = 40
+            this.y = 205
+            this.width = 120
+            this.height=200
+            this.image = new Image()
+            this.image.src = "https://vignette.wikia.nocookie.net/contra/images/f/f6/BillRizerContraNES.png/revision/latest?cb=20171207035053"
+          this.image.onload = () =>{
       this.draw()
     }
-    //this. = new Audio()
-    //this..src = ""
+    this.gravity = 9
+        }
+        draw(){
+          if(this.y < canvas.height - 420) this.y += this.gravity
+            ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
+          }
+}
+
+class Character2{
+  constructor(){
+      this.balas2 = []
+      this.x = 70
+      this.y = 205
+      this.width = 120
+      this.height=200
+      this.image = new Image()
+      this.image.src = "https://vignette.wikia.nocookie.net/contra/images/f/f6/BillRizerContraNES.png/revision/latest?cb=20171207035053"
+    this.image.onload = () =>{
+this.draw()
+}
+this.gravity = 9
   }
   draw(){
-    this.x+=5
-    ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
-  }
-  mosterKill(item){
-    var crash = (this.x < item.x + item.width) &&
-           (this.x + this.width > item.x) &&
-           (this.y < item.y + item.height) &&
-           (this.y + this.height > item.y);
-           //if (crash) this.crash.play()
-           return crash;
-  }
-}
-
-class Pajaros {
-  constructor(y, height){ 
-    this.x = canvas.width -50
-    this.y = y ? y : 0 
-    this.width = 50
-    this.height = height || 100 // si no tines height usa 100
-    this.image = new Image ()
-    this.image.src = images.pajaro
-    this.image.onload = () =>{
-      this.draw()
-    }
-    this.music = new Audio()
-    this.music.src = 'https://sonidosdeanimales.net/wp-content/uploads/2015/09/cuervo.mp3'
-    }
-    draw(){
-      this.x-=2
+    if(this.y < canvas.height - 420) this.y += this.gravity
       ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
-  }
-
+    }
 }
+
+class Balas2{
+  constructor(x=0,y=0){
+    this.x = x
+    this.y = y
+    this.width=50
+    this.height=50
+    this.image = new Image()
+    this.image.src = images.balas
+        }
+        draw(){
+            this.x+=10
+            ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
+        }
+    }
 
 class Zombie {
-  constructor(y, height){ //se ejecuta si no llega
+  constructor(){ 
     this.x = canvas.width -50
-    this.y = y ? y : 0 //si se asigna y si no usa 0
-    this.width = 90
-    this.height = height || 100 // si no tines height usa 100
+    this.y = 380
+    this.width = 150
+    this.height = 200
     this.image = new Image ()
     this.image.src = images.zombie
     this.image.onload = () =>{
@@ -163,139 +147,435 @@ class Zombie {
     this.music.src = "http://soundbible.com/mp3/Mummy Zombie-SoundBible.com-1966938763.mp3"
     }
     draw(){
-      this.x-=.5
+      this.x-=5
       ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
   }
+  checkCollition(soldado){
+            return  (this.x < soldado.x + soldado.width) &&
+                    (this.x + this.width > soldado.x) &&
+                    (this.y < soldado.y + soldado.height) &&
+                    (this.y + this.height > soldado.y);
+        }
 }
 
+class Cuervo {
+  constructor(y){ 
+    this.x = canvas.width +10
+    this.y = y ? y : 0
+    this.width = 80
+    this.height = 100
+    this.image = new Image ()
+    this.image.src = images.pajaro
+    this.image.onload = () =>{
+      this.draw()
+    }
+    this.music = new Audio()
+    this.music.src = "https://sonidosdeanimales.net/wp-content/uploads/2015/09/cuervo.mp3"
+    }
+    draw(){
+      this.x-=15
+      ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
+  }
+  checkCollition(soldado){
+            return  (this.x < soldado.x + soldado.width) &&
+                    (this.x + this.width > soldado.x) &&
+                    (this.y < soldado.y + soldado.height) &&
+                    (this.y + this.height > soldado.y);
+        }
+}
+class Boss {
+  constructor(){ 
+    this.x = canvas.width +10
+    this.y = 80
+    this.width = 350
+    this.height = 500
+    this.image = new Image ()
+    this.image.src = images.boss
+    this.image.onload = () =>{
+      this.draw()
+    }
+    this.music = new Audio()
+    this.music.src = "https://sonidosdeanimales.net/wp-content/uploads/2015/09/cuervo.mp3"
+    }
+    draw(){
+      this.x-=2
+      ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
+  }
+  checkCollition(soldado){
+            return  (this.x < soldado.x + soldado.width) &&
+                    (this.x + this.width > soldado.x) &&
+                    (this.y < soldado.y + soldado.height) &&
+                    (this.y + this.height > soldado.y);
+        }
+}
+
+
+
+class Bomba {
+  constructor(x){ 
+    this.x = x ? x : 0
+    this.y = -100
+    this.width = 80
+    this.height = 80
+    this.image = new Image ()
+    this.image.src = images.bomba
+    this.image.onload = () =>{
+      this.draw()
+    }
+    }
+    draw(){
+      this.y+=5
+      ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
+  }
+  checkCollition(soldado){
+            return  (this.x < soldado.x + soldado.width) &&
+                    (this.x + this.width > soldado.x) &&
+                    (this.y < soldado.y + soldado.height) &&
+                    (this.y + this.height > soldado.y);
+        }
+}
+/////////////////termina clases
 
 //instancias
-var foondo = new Fondo()
-var pisoo = new Piso()
-var comand = new Comandante()
-var zombieee = new Zombie()
-var cuervo = new Pajaros()
-var balasas = new Balas()
+var chipHasard = new Character()
+var piso = new Piso()
+var zombie = new Zombie()
+var fondo = new Fondo()
+var cuervo = new Cuervo()
+var bomba = new Bomba()
+var boss = new Boss()
+var gorgonita = new Character2()
 
-//funciones principales
-function update(){
+
+///////funciones principales
+function update (){
   frames++
   ctx.clearRect(0,0,canvas.width,canvas.height)
-  foondo.draw()
-  pisoo.draw()
-  comand.draw()
-  //////////////////
-  generatePajaros()
-  drawPajaros()
-  generateZombies()
-  drawZombies()
-  checkCollitions()
+  fondo.draw()
+  piso.draw()
+  chipHasard.draw()
+  gorgonita.draw()
+  
+  ///////////////////////////////
+  generateZombie()
+  drawZombie()
+  checkZombiesCollitions()
+
+  checkZombiesCollitions2()
+  ////////////////////////////
+  generateCuervo()
+  drawCuervo()
+  checkCuervosCollitions()
+
+  checkCuervosCollitions2()
+  //////////////////////////////
+  generateBomba()
+  drawBombas()
+  checkBombasCollitions()
+
+  checkBombasCollitions2()
+  /////////////////////////////
+ // if(score>10)drawBoss()
+  //balasAndBadBoss()
+
+  //balasAndBadBoss2()
+  ////////////////////////////
   drawBalas()
-  killMonsters()
+  balasAndBadZombies()
+  balasAndBadCuervos()
+
+  drawBalas2()
+  balasAndBadZombies2()
+  balasAndBadCuervos2()
+  ////////////////////////////
+  drawScore1()
+  drawScore2()
 }
+
 
 function start(){
   if(interval) return
- 
   interval = setInterval(update,1000/60)
 }
 
-function gameOver(){
+/*function gameOver(){
   clearInterval(interval)
   ctx.font = ('90px Avenir')
   ctx.fillText('Game Over', 50 ,250)
   interval=null
-  comand.crash.play()
-  pisoo.music.pause()
+}*/
 
-}
-
-//funciones auxiliares
-function generatePajaros(){
-  if(frames % 250 === 0){
-      var y = Math.floor(Math.random() * 400) + 20;
-      var alto = 40;
-      var cuervo = new Pajaros(y,alto, 'pajaro')
-      cuervos.push(cuervo)
-      cuervo.music.play()
-  }
-}
-
-function drawPajaros(){
-  cuervos.forEach(function(pajaro){
-    pajaro.draw()
-  })
-}
-
-function generateZombies(){
-  if(frames % 150 === 0){
-      var y = 260;
-      var alto = 100;
-      var zombies = new Zombie(y,alto, 'zombie')
-      zombiess.push(zombies)
-      zombieee.music.play()
-  }
-}
-
-function drawZombies(){
-  zombiess.forEach(function(zombie){
-    zombie.draw()
-  })
-}
-
-function killMonsters(){
-  disparosP.forEach(function(zombie){
-    if(balasas.mosterKill(zombie)){
-    zombiess.splice(0,1);
-  }
-})
-}
-
-function checkCollitions(){
-  zombiess.forEach(function(zombie){
-    if(comand.crashWidth(zombie)){
-      gameOver()
+////funciones auxiliares
+function balasAndBadZombies(){
+        chipHasard.balas.forEach(function(bala, bI){
+            zombies.forEach(function(zombie, zI){
+                if(zombie.checkCollition(bala)){
+                    chipHasard.balas.splice(bI,1)
+                    zombies.splice(zI,1)
+                    zombie.music.play()
+                    score1 ++
+                    if(score1 > 5 ) {
+                      clearInterval(interval)
+                      ctx.font = "30px 'Press Start 2P', cursive";
+                      ctx.fillStyle = "black";
+                      ctx.fillText("Mission Complete", 400, 300);
+                      ctx.fillText("score player 1: "+score1 + "!!!", 400,350)
+                      interval=null
+                }
+              }
+            })
+          
+        })
     }
-  })
-  cuervos.forEach(function(cuervo){
-    if(comand.crashWidth(cuervo)){
-      gameOver()
-    }
-  })
 
+    function balasAndBadBoss(){
+      chipHasard.balas.forEach(function(bala, bI){
+              if(boss.checkCollition(bala)){
+                  chipHasard.balas.splice(bI,1)
+                  score1 ++
+              }
+          })
+  }
+  
+  
+    function balasAndBadCuervos(){
+      chipHasard.balas.forEach(function(bala, bI){
+          cuervos.forEach(function(cuervo, cI){
+              if(cuervo.checkCollition(bala)){
+                  chipHasard.balas.splice(bI,1)
+                  cuervo.splice(cI,1)
+                  cuervo.music.play()
+                  score1 ++
+              }
+          })
+      })
+  }
+
+  function drawScore1() {
+    ctx.font = "30px 'Press Start 2P', cursive";
+    ctx.fillStyle = "red";
+    ctx.fillText("Score player 1: "+score1, 38, 50);
 }
 
-function generateBalas(){
-  var balas = new Balas('balas')
-  disparosP.push(balas)
+function drawScore2() {
+  ctx.font = "30px 'Press Start 2P', cursive";
+  ctx.fillStyle = "red";
+  ctx.fillText("Score player 2: "+score2, 1300, 50);
+}
+
+  function checkCuervosCollitions(){
+    cuervos.forEach(function(cuervo){
+        if(cuervo.checkCollition(chipHasard)){
+            var index = cuervos.indexOf(cuervo)
+            cuervos.splice(index,1)
+        }
+    })
+}
+
+function checkBombasCollitions(){
+  bombas.forEach(function(bomba){
+      if(bomba.checkCollition(chipHasard)){
+          var index = bombas.indexOf(bomba)
+          bombas.splice(index,1)
+      }
+  })
+}
+
+function drawBombas(){
+  bombas.forEach(function(bomba){
+      bomba.draw()
+  })
 }
 
 function drawBalas(){
-  disparosP.forEach(function(bala){
-    bala.draw()
-  })
+        chipHasard.balas.forEach(function(bala){
+            bala.draw()
+        })
+    }
+
+    function drawBoss(){
+      
+          boss.draw()
+    
+  }
+
+function checkZombiesCollitions(){
+        zombies.forEach(function(zombie){
+            if(zombie.checkCollition(chipHasard)){
+                var index = zombies.indexOf(zombie)
+                zombies.splice(index,1)
+            }
+        })
+    }
+
+function drawZombie(){
+        zombies.forEach(function(zombie){
+            zombie.draw()
+        })
+    }
+
+
+    //////////////////////////////////////////////////////////////////////player2
+
+    function balasAndBadZombies2(){
+      gorgonita.balas2.forEach(function(bala2, bI){
+          zombies.forEach(function(zombie, zI){
+              if(zombie.checkCollition(bala2)){
+                  gorgonita.balas2.splice(bI,1)
+                  zombies.splice(zI,1)
+                  zombie.music.play()
+                  score2 ++
+                  if(score2 > 5 ) {
+                    clearInterval(interval)
+                    ctx.font = "30px 'Press Start 2P', cursive";
+                    ctx.fillStyle = "black";
+                    ctx.fillText("Mission Complete", 400, 300);
+                    ctx.fillText("score player 2: "+score2 + "!!!", 400,350)
+                    interval=null
+              }
+            }
+          })
+        
+      })
+  }
+
+    function balasAndBadBoss2(){
+      gorgonita.balas2.forEach(function(bala2, bI){
+              if(boss.checkCollition(bala2)){
+                  gorgonita.balas2.splice(bI,1)
+                  score2 ++
+              }
+          })
+  }
+
+    function balasAndBadCuervos2(){
+      gorgonita.balas2.forEach(function(bala2, bI){
+          cuervos.forEach(function(cuervo, cI){
+              if(cuervo.checkCollition(bala2)){
+                  chipHasard.balas2.splice(bI,1)
+                  cuervo.splice(cI,1)
+                  cuervo.music.play()
+                  score2 ++
+              }
+          })
+      })
+  }
+
+
+    function checkCuervosCollitions2(){
+      cuervos.forEach(function(cuervo){
+          if(cuervo.checkCollition(gorgonita)){
+              var index = cuervos.indexOf(cuervo)
+              cuervos.splice(index,1)
+          }
+      })
+  }
+
+
+    function checkBombasCollitions2(){
+      bombas.forEach(function(bomba){
+          if(bomba.checkCollition(gorgonita)){
+              var index = bombas.indexOf(bomba)
+              bombas.splice(index,1)
+          }
+      })
+    }
+
+
+    function checkZombiesCollitions2(){
+      zombies.forEach(function(zombie){
+          if(zombie.checkCollition(gorgonita)){
+              var index = zombies.indexOf(zombie)
+              zombies.splice(index,1)
+          }
+      })
+  }
+
+    function drawBalas2(){
+      gorgonita.balas2.forEach(function(bala2){
+          bala2.draw()
+      })
+  }
+    ///////////////////////////////////////////////////////////////////////////////
+
+
+    function drawCuervo(){
+      cuervos.forEach(function(cuervo){
+          cuervo.draw()
+      })
+  }
+
+function generateZombie(){
+        if(frames % 200 === 0){
+            zombies.push(new Zombie())
+        }  
+    }
+
+    function generateCuervo(){
+      if(frames % 50 === 0){
+        var y = Math.floor(Math.random() * 350) + 80;
+        var cuervo = new Cuervo(y)
+          zombies.push(cuervo)
+      }  
+  }
+
+  function generateBomba(){
+    if(frames % 50 === 0){
+      var x = Math.floor(Math.random() * 1600) + 80;
+      var bomba = new Bomba(x)
+        bombas.push(bomba)
+    }  
 }
 
 
-//los observadores
-  addEventListener('keydown',function(e){
-    if(e.keyCode === 32 && comand.y > 30){
-      comand.y -= 200
-    }
-    if(e.keyCode == 90){
-      generateBalas(comand);
-    }
-    if(e.keyCode == 39){
-      comand.goRight()
-    }
-    if(e.keyCode == 37){
-      comand.goLeft()
-    }
-    if(e.key == 'Enter'){
-      start()
-     pisoo.music.play()
-    }
-  })
+///Observadores    
+addEventListener('keydown',function(e){
+  if(e.keyCode === 32){
+    chipHasard.balas.push(new Balas(chipHasard.x+100,chipHasard.y+45))
+  } 
+  if(e.keyCode === 38 && chipHasard.y > 350){
+    chipHasard.y -= 400
+  }
+  if(e.keyCode == 39){
+    chipHasard.x+=39
+  }
+  if(e.keyCode == 37){
+    chipHasard.x-=15
+  }
+  if(e.key == 'Enter'){
+    start()
+    fondo.music.play()
+  }
+})
+
+
+addEventListener('keydown',function(e){
+  if(e.keyCode === 16){
+    gorgonita.balas2.push(new Balas2(gorgonita.x+100,gorgonita.y+45))
+  } 
+  if(e.keyCode === 87 && gorgonita.y > 350){
+    gorgonita.y -= 400
+  }
+  if(e.keyCode == 68){
+    gorgonita.x+=39
+  }
+  if(e.keyCode == 65){
+    gorgonita.x-=15
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -308,3 +588,4 @@ function drawBalas(){
   // zelda enemy http://66.90.93.122/ost/legend-of-zelda-the-a-link-to-the-past-gb/cznvhyve/26%20Release%20of%20Ganon.mp3
 
   // zombie "http://soundbible.com/mp3/Mummy Zombie-SoundBible.com-1966938763.mp3"
+  
